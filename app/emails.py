1a25338fad26615edc2234e0f5aad68095e32b51
Jyoti Sheoran <sheoranjs24@gmail.com>
2015-03-05 19:24:21 -0800

    async call for sending emails

--- app/emails.py
+++ app/emails.py
@@ -1,13 +1,19 @@
+from threading import Thread
 from flask.ext.mail import Message
 from flask import render_template
 from config import ADMINS
 from app import mail
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+        
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
 
 def follower_notification(followed, follower):
     send_email("[microblog] %s is now following you!" % follower.nickname,
