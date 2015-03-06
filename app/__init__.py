0f39adcf5b5321d4afe6af99e1a776a7768da368
Jyoti Sheoran <sheoranjs24@gmail.com>
2015-03-05 21:58:30 -0800

    display local time

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from flask.ext.mail import Mail
-from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, \
-    MAIL_PASSWORD
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -15,6 +15,7 @@ lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 mail = Mail(app)
+app.jinja_env.globals['momentjs'] = momentjs
 
 if not app.debug:
     import logging
