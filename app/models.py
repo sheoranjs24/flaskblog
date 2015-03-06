bfdb96a2c4dd59d8ce8dadb02e3112e41c1e182a
Jyoti Sheoran <sheoranjs24@gmail.com>
2015-03-05 19:10:07 -0800

    full text search

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
 from hashlib import md5
-from app import db
+from app import db, app
 
+import sys
+if sys.version_info >= (3, 0):
+    enable_search = False
+else:
+    enable_search = True
+    import flask.ext.whooshalchemy as whooshalchemy
+    
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
@@ -70,6 +77,8 @@ class User(db.Model):
         return '<User %r>' % (self.nickname)
 
 class Post(db.Model):
+    __searchable__ = ['body']
+    
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
@@ -77,3 +86,6 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+if enable_search:
+    whooshalchemy.whoosh_index(app, Post)
