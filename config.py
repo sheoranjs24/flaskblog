59c9c430b41ee28d3bf95e76bbea11d6c77dda60
Jyoti Sheoran <sheoranjs24@gmail.com>
2015-03-07 17:54:37 -0800

    update database config

--- config.py
+++ config.py
@@ -12,12 +12,18 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                               '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+
 # Whoosh does not work on Heroku
 WHOOSH_ENABLED = os.environ.get('HEROKU') is None
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
-SQLALCHEMY_RECORD_QUERIES = True
+
 # slow database query threshold (in seconds)
 DATABASE_QUERY_TIMEOUT = 0.5
 
@@ -33,7 +39,7 @@ MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 ADMINS = ['sheoranjs24@gmail.com']
 
 # pagination
-POSTS_PER_PAGE = 3
+POSTS_PER_PAGE = 10
 MAX_SEARCH_RESULTS = 50
 
 # available languages
